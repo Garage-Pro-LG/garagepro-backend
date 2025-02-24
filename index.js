f74e13f01f73cb09588aefc21cfc4f50e3ebc4cb
david_ag <guayo178@gmail.com>
2025-02-24 12:07:32 +0100

    prepare Railway connection

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Servidor funcionando en Railway!');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
+});
